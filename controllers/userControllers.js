fb712206fde70f8f86a86f5b65707ef84ad6bc55
RitikKumar202 <ritik2002july@gmail.com>
2023-08-18 14:59:05 +0530

    implemented users profile functionality

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,14 @@
+import { uploadPicture } from "../middlewares/uploadPictureMiddleware";
 import User from "../models/User";
+import { fileRemover } from "../utils/fileRemover";
 
 const registerUser = async (req, res, next) => {
   try {
     const { email, name, password } = req.body;
+
+    if (password.length < 6) {
+      throw new Error("Password length must be at least 6 character");
+    }
 
     // check whether user exists or not
     let user = await User.findOne({ email });
@@ -79,4 +85,96 @@ const userProfile = async (req, res, next) => {
   }
 };
 
-export { registerUser, loginUser, userProfile };
+const updateProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password && req.body.password.length < 6) {
+      throw new Error("Password length must be at least 6 character");
+    } else if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUserProfile = await user.save();
+
+    res.json({
+      _id: updatedUserProfile._id,
+      avatar: updatedUserProfile.avatar,
+      name: updatedUserProfile.name,
+      email: updatedUserProfile.email,
+      verified: updatedUserProfile.verified,
+      admin: updatedUserProfile.admin,
+      token: await updatedUserProfile.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateProfilePicture = async (req, res, next) => {
+  try {
+    const upload = uploadPicture.single("profilePicture");
+
+    upload(req, res, async function (err) {
+      if (err) {
+        const error = new Error(
+          "An unknown error occured while uploading " + err.message
+        );
+        next(error);
+      } else {
+        // every thing went well
+        if (req.file) {
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          if (filename) {
+            fileRemover(filename);
+          }
+          updatedUser.avatar = req.file.filename;
+          await updatedUser.save();
+          res.json({
+            _id: updatedUser._id,
+            avatar: updatedUser.avatar,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            verified: updatedUser.verified,
+            admin: updatedUser.admin,
+            token: await updatedUser.generateJWT(),
+          });
+        } else {
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          updatedUser.avatar = "";
+          await updatedUser.save();
+          fileRemover(filename);
+          res.json({
+            _id: updatedUser._id,
+            avatar: updatedUser.avatar,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            verified: updatedUser.verified,
+            admin: updatedUser.admin,
+            token: await updatedUser.generateJWT(),
+          });
+        }
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  userProfile,
+  updateProfile,
+  updateProfilePicture,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 import {
   loginUser,
   registerUser,
+  updateProfile,
+  updateProfilePicture,
   userProfile,
 } from "../controllers/userControllers";
 import { authGuard } from "../middlewares/authMiddleware";
@@ -10,5 +12,7 @@ import { authGuard } from "../middlewares/authMiddleware";
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
+router.put("/updateProfilePicture", authGuard, updateProfilePicture);
 
 export default router;
