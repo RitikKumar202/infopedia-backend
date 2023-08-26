bf1f98e2210a9afde95f14723676997e168164cc
RitikKumar202 <ritik2002july@gmail.com>
2023-08-26 17:00:13 +0530

    implemented update & delete comment functionality

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
 const router = express.Router();
 import { authGuard } from "../middlewares/authMiddleware";
-import { createComment } from "../controllers/commentControllers";
+import {
+  createComment,
+  deleteComment,
+  updateComment,
+} from "../controllers/commentControllers";
 
 router.post("/", authGuard, createComment);
+router
+  .route("/:commentId")
+  .put(authGuard, updateComment)
+  .delete(authGuard, deleteComment);
 
 export default router;
